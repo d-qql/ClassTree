62a103ce94233081b4eb677711be58bc37a9f683
d-qql <58688832+d-qql@users.noreply.github.com>
2020-02-05 15:06:03 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/2sem05_02_2020.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2sem05_02_2020.dir/main.cpp.obj"
+  "2sem05_02_2020.pdb"
+  "2sem05_02_2020.exe"
+  "2sem05_02_2020.exe.manifest"
+  "lib2sem05_02_2020.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2sem05_02_2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
